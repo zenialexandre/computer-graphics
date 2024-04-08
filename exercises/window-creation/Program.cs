1b01e0b0e4f7b9851278175a7df715bdfbefab79
zenialexandre <zenialexandre123@gmail.com>
2024-04-08 09:05:04 -0300

    refactor: adjusting namespace and import

---
+++ exercises/window-creation/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+
+namespace WindowCreation
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                ClientSize = new Vector2i(800, 600),
+                Title = "Window Creation Example"
+            };
+
+            using var window = new Window(GameWindowSettings.Default, nativeWindowSettings);
+            window.Run();
+        }
+    }
+}
