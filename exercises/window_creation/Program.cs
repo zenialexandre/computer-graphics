59f094549486a24adc550e76203062633a763e00
zenialexandre <zenialexandre123@gmail.com>
2024-04-08 09:02:30 -0300

    feat: window opening exercise

---
+++ exercises/window_creation/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace window_creation
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                ClientSize = new Vector2i(800, 600),
+                Title = "Window Creation Example"
+            };
+
+            using var window = new Window(GameWindowSettings.Default, nativeWindowSettings);
+            window.Run();
+        }
+    }
+}
